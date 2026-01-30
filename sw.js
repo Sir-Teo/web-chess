55a443e1d0609437298c01d307b8fbe818514726
Teo <32597645+Sir-Teo@users.noreply.github.com>
2026-01-30 16:05:54 -0500

    Bump service worker cache to v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CORE_CACHE = "vulcan-core-v2";
-const RUNTIME_CACHE = "vulcan-runtime-v2";
+const CORE_CACHE = "vulcan-core-v3";
+const RUNTIME_CACHE = "vulcan-runtime-v3";
 
 const CORE_ASSETS = [
   "./",
